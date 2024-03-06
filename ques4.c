e297b02ba5669eeb0ab3d8bebaa2b48d00803fa5
Deathviser <161715295+Deathviser@users.noreply.github.com>
2024-03-06 21:33:25 +0530

    Create ques4.c

---
+++ ques4.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int findmaxinrow(int ar[][100], int r,int c);
+int main()
+{
+    int a[100][100],r,c,i,j;
+    printf ("Enter number of rows and columns\n");
+    scanf("%d%d",&r,&c);
+    printf("Enter elements\n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Entered matrix is\n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    findmaxinrow(a,r,c);
+    return 0;
+}
+
+
+
+int findmaxinrow(int a[][100],int r,int c)
+{
+    int i,j,max;
+    max=a[0][0];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(a[i][j]>max)
+            {
+                max=a[i][j];
+            }
+        }
+        printf("the maximum in the row %d is=%d\n",i+1,max);
+    }
+    return 0;
+}
